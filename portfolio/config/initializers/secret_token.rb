1ba940fb0d942e9402403faa322e8d1e6ee536fc
Viet <vtn1989@gmail.com>
2014-02-25 19:25:23 -0500

    initial commit

---
+++ portfolio/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = '237dcc3e313022f0f039fcbfe86fcde7bb43c0e0ea86a92eb2577fab3a10f869ee2b380603d98b4c5ac9cf0c924241c627de60122aaafedc24cf787f18ebf9c2'
